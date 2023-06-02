07cb4832301a3b6392f51137e8375e8ed67b60ea
vitorboos1 <125585345+vitorboos1@users.noreply.github.com>
2023-06-02 11:14:24 -0300

    Add files via upload

---
+++ Banco de Dados e Modelagem Lógica Projeto Individual/Banco de Dados Projeto Individual.sql
@@ -0,0 +1,19 @@
+create database Projeto_individual;
+use Projeto_individual;
+
+create table usuario (
+idUsuario int primary key auto_increment,
+nomeUsuario varchar(45),
+estadoUsuario varchar(45));
+
+create table pokemon (
+idPokemon int primary key auto_increment,
+nomePokemon varchar(45));
+
+create table PokemonUsuario (
+fkPokemon int,
+fkUsuario int, 
+qtdVotos int,
+primary key (fkPokemon, fkUsuario),
+foreign key (fkPokemon) references pokemon(idPokemon),
+foreign key (fkUsuario) references usuario(idUsuario));
